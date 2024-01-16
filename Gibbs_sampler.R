fe58fbadb8208a26b9e7ee68fb355ccac5c31fba
Yang_Q <114128631+YNWA2022@users.noreply.github.com>
2024-01-16 11:14:37 -0500

    changes reduce empty lines

--- Gibbs_sampler.R
+++ Gibbs_sampler.R
@@ -23,6 +23,32 @@ hist(mysamples$p)
 hist(mysamples$N)
 
 #1/11/2024_________________________________________________________________________________________________________
-#test codes
+# data preprocessing steps......
+install.packages("dplyr")
+set.seed(1)
+x1<-matrix(c(rep(1, 100)), ncol=1)
+x2_x3 <- matrix(rnorm(200), nrow = 100, ncol = 2)
+x <- cbind(x1, x2_x3)
+eps<-matrix(rnorm(100),nrow =100, ncol=1)
+beta <- matrix(c(1, 2, 0.5), ncol = 1)
+y<-x%*%beta+eps
+
+#computation steps prepare for prior beta and sigma2
+sigma2 <- 1
+gamma<-solve((diag(3)+(1/sigma2)*t(x)%*%x))%*%t(x)%*%y
+omega<-solve((diag(3)+(1/sigma2)*t(x)%*%x))
+
+
+#initial values j[0]
+
+
+
+#Gibbs algorithms 
+for(j in 2:(J+n+1)) {
+  x.seq[j] <- rbinom(1, N.seq[j-1], p.seq[j -1])
+  p.seq[j] <- rbeta(1, (x.seq[j] + 2),
+                    (N.seq[j -1] - x.seq[j] + 4))
+  N.seq[j] <- rpois(1, 16 * (1 - p.seq[j])) + x.seq[j]
+}
 
 
