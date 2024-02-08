e8966bd37a615481353483b83d734d74842de465
Yang_Q <114128631+YNWA2022@users.noreply.github.com>
2024-02-08 14:57:13 -0500

    added on 2/8/2024

---
+++ dataGeneration.R
@@ -0,0 +1,21 @@
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("MASS")
+install.packages("Matrix")
+install.packages("stats4")
+install.packages("gmm")
+install.packages("sandwich")
+install.packages("tmvtnorm")
+install.packages("crch")
+install.packages("coda")
+install.packages("MCMCpack")
+library("stats")
+
+# data preprocessing steps--------------------------------------------------------------------
+set.seed(100)
+x1<-matrix(c(rep(1, 100)), ncol=1)
+x2_x3 <- matrix(rnorm(200), nrow = 100, ncol = 2)
+x <- cbind(x1, x2_x3)
+eps<-matrix(rnorm(100),nrow =100, ncol=1)
+beta_1 <- matrix(c(1, 2, 0.5), ncol = 1)
+y<-x%*%beta+eps
